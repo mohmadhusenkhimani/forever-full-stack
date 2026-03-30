0fd656f70bcfacc37fb23135fd01da46c421ef10
Mohmadhusen Khimani <143099310+mohmadhusenkhimani@users.noreply.github.com>
2026-03-30 22:23:42 +0530

    first commit

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewslatterBox from '../components/NewsletterBox'
+
+const About = () => {
+  return (
+    <div>
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1={'ABOUT'} text2={'US'}/>
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+        <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque odio dolor, delectus non voluptatem esse perferendis ratione? Quis sequi modi impedit eveniet autem, culpa quasi facilis molestiae natus soluta aut possimus maxime explicabo quaerat., culpa quasi facilis molestiae natus  maxime explicabo quaerat.</p>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos quaerat nostrum, ab saepe delectus expedita quidem corrupti laboriosam facere animi vel ut ea voluptas amet?, ab saepe delectus expedita quidem corrupti laboriosam facere animi vel ut ea voluptas amet voluptas amet?</p>
+          <b className='text-gray-800'>Our Mission</b>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam ab quos quidem voluptates? Dolore natus amet cumque corrupti magnam porro.Dolore natus amet cumque corrupti magnam porro.Dolore natus amet.</p>
+        </div>
+      </div>
+
+      <div className='text-xl py-4'>
+        <Title text1={'WHY'} text2={'CHOOSE US'}/>
+      </div>
+
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Quality Assurance:</b>
+          <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, ipsum?</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Convenience:</b>
+          <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, ipsum?</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Exceptional Customer Service:</b>
+          <p className='text-gray-600'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, ipsum?</p>
+        </div>
+      </div>
+
+      <NewslatterBox />
+
+    </div>
+  )
+}
+
+export default About
